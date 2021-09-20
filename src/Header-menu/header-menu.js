a0635e3da4fce31a6666a4992231967dc31375bb
Ax51 <80718359+Ax51@users.noreply.github.com>
2021-09-21 00:35:56 +0300

    Map API added, callback button added, scrollUp button added and so on

--- src/Header-menu/header-menu.js
+++ src/Header-menu/header-menu.js
@@ -1,44 +1,69 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import './header-menu.css';
 
 import logo from '../img/mz_logo-name.2c83.png'
+import phone from '../img/telephone-fill__white.png'
 import menu from '../img/mz_menu-17.3a29.png'
+import arrowUp from '../img/arrow-up-circle.png'
 
 const HeaderMenu = () => {
+    const myRef = useRef(null);
+    const scrollUpFunc = () => myRef.current.scrollIntoView()
     return (
-        <header className="header-menu">
-            <div className="header-menu__logo">
+        <>
+        <div ref={myRef}/>  {/* div with zero position to scroll to */}
+            <header className="header-menu">
+                <div className="header-menu__logo">
+                    <img
+                        src={logo}
+                        alt='logo' />
+                </div>
+                {window.innerWidth > 1000 ?
+                    <>
+                        <div className="header-menu__menu-section">
+                            <ul>
+                                <li><a href="#">Стоимость</a></li>
+                                <li><a href="#">Практики</a></li>
+                                <li><a href="#">Адвокатская практика</a></li>
+                                <li><a href="#">Карьера</a></li>
+                                <li><a href="#">Контакты</a></li>
+                            </ul>
+                        </div>
+                        <div className="header-menu__phones">
+                            <ul>
+                                <li><a href="tel:+74957696889">8(495)769-68-89</a></li>
+                                <li><a href="tel:+74992526522">8(499)252-65-22</a></li>
+                            </ul>
+                        </div>
+                    </> :
+                    <div className="header-menu__menu-burger">
+                        <a href="tel:+74957696889">
+                            <button>
+                                <img
+                                    src={phone}
+                                    alt="call us" />
+                            </button>
+                        </a>
+                        <a href="#">
+                            <button>
+                                <img
+                                    src={menu}
+                                    alt='menu' />
+                            </button>
+                        </a>
+                    </div>}
+            </header>
+            <a href="#">
+                <div className="callback-btn">Заказать звонок</div>
+            </a>
+            <button className="scroll-up-btn"
+                onClick={scrollUpFunc}>
                 <img
-                    src={logo}
-                    alt='logo' />
-            </div>
-            {window.innerWidth > 1000 ?
-                <>
-                    <div className="header-menu__menu-section">
-                        <ul>
-                            <li><a href="#">Стоимость</a></li>
-                            <li><a href="#">Практики</a></li>
-                            <li><a href="#">Адвокатская практика</a></li>
-                            <li><a href="#">Карьера</a></li>
-                            <li><a href="#">Контакты</a></li>
-                        </ul>
-                    </div>
-                    <div className="header-menu__phones">
-                        <ul>
-                            <li><a href="tel:+74957696889">8(495)769-68-89</a></li>
-                            <li><a href="tel:+74992526522">8(499)252-65-22</a></li>
-                        </ul>
-                    </div>
-                </> :
-                <div className="header-menu__menu-burger">
-                    <a href="#">
-                        <img
-                            src={menu}
-                            alt='menu' />
-                    </a>
-                </div>}
-        </header>
+                    src={arrowUp}
+                    alt='scroll up' />
+            </button>
+        </>
     );
 };
 
--- src/Map-API/Map-API.js
+++ src/Map-API/Map-API.js
@@ -1,11 +1,126 @@
 import React from "react";
 
+import { YMaps, Map, Placemark, ZoomControl } from 'react-yandex-maps';
+
 import './Map-API.css';
+
+// Ymaps API keys:
+// 1) 08214da6e825bb8b4e37599b03c92015858b2316
+// 2) 4abe14abc9b3e46c
+// 3) 3Aa5ef8364e7502fbfcf8ea8ab9e7fada5c2752129afd88ddbd71a807f2dfe1378
 
 const MapApi = () => {
     return (
         <div className="map-api">
-            this is Map-API.trust me
+            <YMaps
+                lang={'ru_RU'}>
+                <div className="map-container">
+                    <Map
+                        defaultState={{
+                            center: [55.767379, 37.584293],
+                            zoom: 18
+                        }}
+                        width={'100%'}
+                        height={400}>
+                        <Placemark
+                            // Филиал № 2
+                            // г. Москва, Рубцовская набережная, д. 4, корп. 3
+                            geometry={[55.775819, 37.698538]}
+                            properties={{
+                                iconCaption: 'Филиал №2'
+                            }}
+                            options={{
+                                preset: 'islands#blueCircleDotIconWithCaption',
+                                draggable: false,
+                                iconCaptionMaxWidth: 120
+                            }} />
+                        <Placemark
+                            // Филиал № 3
+                            // г. Москва, ?????
+                            // geometry={[55.775819, 37.698538]}
+                            properties={{
+                                iconCaption: 'Филиал №3'
+                            }}
+                            options={{
+                                preset: 'islands#blueCircleDotIconWithCaption',
+                                draggable: false,
+                                iconCaptionMaxWidth: 120
+                            }} />
+                        <Placemark
+                            // Филиал № 4
+                            // г. Москва, ул.  Студенческая, 26/3 стр.1
+                            geometry={[55.742448, 37.546390]}
+                            properties={{
+                                iconCaption: 'Филиал №4'
+                            }}
+                            options={{
+                                preset: 'islands#blueCircleDotIconWithCaption',
+                                draggable: false,
+                                iconCaptionMaxWidth: 120
+                            }} />
+                        <Placemark
+                            // Филиал № 5
+                            // Московская обл., г. Солнечногорск, ул. Советская, д.9, кв.7
+                            geometry={[56.185873, 36.977101]}
+                            properties={{
+                                iconCaption: 'Филиал №5'
+                            }}
+                            options={{
+                                preset: 'islands#blueCircleDotIconWithCaption',
+                                draggable: false,
+                                iconCaptionMaxWidth: 120
+                            }} />
+                        <Placemark
+                            // Филиал № 8
+                            // гор. Зеленоград, Заводская 16 а, офис 3
+                            geometry={[55.976398, 37.178700]}
+                            properties={{
+                                iconCaption: 'Филиал №8'
+                            }}
+                            options={{
+                                preset: 'islands#blueCircleDotIconWithCaption',
+                                draggable: false,
+                                iconCaptionMaxWidth: 120
+                            }} />
+                        <Placemark
+                            // Филиал № 18
+                            // гор. Москва, ул. Радужная, д.11 кв.61
+                            geometry={[55.859802, 37.664860]}
+                            properties={{
+                                iconCaption: 'Филиал №18'
+                            }}
+                            options={{
+                                preset: 'islands#blueCircleDotIconWithCaption',
+                                draggable: false,
+                                iconCaptionMaxWidth: 120
+                            }} />
+                        <Placemark
+                            // Филиал Интерлоер
+                            // ггор. Москва,  ул. Кутузова, д. 2,/111
+                            geometry={[55.720672, 37.417643]}
+                            properties={{
+                                iconCaption: 'Филиал №18'
+                            }}
+                            options={{
+                                preset: 'islands#blueCircleDotIconWithCaption',
+                                draggable: false,
+                                iconCaptionMaxWidth: 120
+                            }} />
+                        <Placemark
+                            // Центральный офис + филиал № 1
+                            // г. Москва, ул. Зоологическая, д.30 стр. 2, пом /офис III
+                            geometry={[55.767379, 37.584293]}
+                            properties={{
+                                iconContent: 'МКА "Защита"',
+                            }}
+                            options={{
+                                preset: 'islands#lightBlueStretchyIcon',
+                                draggable: false
+                            }} />
+                        <ZoomControl />
+                    </Map>
+                </div>
+            </YMaps>
         </div>
     );
 };
