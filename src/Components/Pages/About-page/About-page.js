8ea62712af472093598d3ba1496ddeabe740ebaf
Ax51 <80718359+Ax51@users.noreply.github.com>
2021-09-26 15:05:35 +0300

    minor changes. test git

---
+++ src/Components/Pages/About-page/About-page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+    return (
+        <div className="about-page">
+            this is about page. trust me
+        </div>
+    )
+}
+
+export default AboutPage;
