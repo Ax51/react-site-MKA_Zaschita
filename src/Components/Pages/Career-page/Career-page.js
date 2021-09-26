de8d4be6b8b64455b58359c8249737f694d76948
Ax51 <80718359+Ax51@users.noreply.github.com>
2021-09-26 16:54:24 +0300

    added Career page

--- src/Components/Pages/Career-page/Career-page.js
+++ src/Components/Pages/Career-page/Career-page.js
@@ -1,5 +1,9 @@
 // modules
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+// pics
+import arrowRight from "../../img/arrow-right.png";
 
 // styles
 import './Career-page.css';
@@ -7,7 +11,29 @@ import './Career-page.css';
 const CareerPage = () => {
     return (
         <div className="career-page">
-            this is Career page. trust me
+            <div className="career-page_nav">
+                <NavLink
+                    className="_nav-main"
+                    to='/'>Главная</NavLink>
+                <img
+                    className="_nav-route_img"
+                    src={arrowRight}
+                    alt="path" />
+                <div className="_nav-this">
+                    Карьера
+                </div>
+            </div>
+            <div className="career-page_header">
+                <h1>Информация для будущих помощников / стажеров</h1>
+                <div className="header-block header-block_dark" />
+            </div>
+            <div className="career-page_text">
+                <p>МКА «ЗАЩИТА» производит набор помощников/стажеров адвокатов на конкурсной основе. Практическое и теоретическое обучение профессии, оформление по ТК РФ, стаж по юридической специальности, необходимый для допуска к сдаче квалификационного экзамена, подготовка к сдаче квалификационных экзаменов на статус адвоката в Адвокатской Палате г. Москвы и Московской области. Для получения более подробной информации, <NavLink to="/contacts">свяжитесь с нами.</NavLink></p>
+                <ul>
+                    <li><a href="https://fparf.ru/documents/fpa-rf/the-documents-of-the-council/regulations-on-the-procedure-of-internship/">Положение о порядке прохождения стажировки</a></li>
+                    <li><a href="https://fparf.ru/documents/fpa-rf/the-documents-of-the-council/regulations-on-the-procedure-of-work-of-the-assistant-to-the-lawyer/?sphrase_id=80015">Положение о порядке работы помощника адвоката</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
