de482580065d695e0db645a9a0a27f267e9b8d99
Ax51 <80718359+Ax51@users.noreply.github.com>
2021-11-08 00:01:54 +0300

    added propper order

--- src/Db/Map-API-Db/Map-coordinates.js
+++ src/Db/Map-API-Db/Map-coordinates.js
@@ -1,63 +1,13 @@
 const coordinates = {
-    branch_2:{
-        geometry:[55.775819, 37.698538],
+    branch_femida:{
+        geometry:[55.787973, 37.511266],
         iconContent:'',
-        iconCaption:"Филиал №2",
+        iconCaption:"Филиал Фемида",
         preset:"islands#blueCircleDotIconWithCaption",
-        balloonText:"Заведующая филиалом № 2:<br><b>Трегуб Ольга Анатольевна</b>",
-        link:"",
-        balloonImg:"https://avatars.mds.yandex.net/get-altay/859900/2a0000015f57ecdcf9997bcd4ad7e1420b57/XXXL",
-        comment:"Филиал № 2, г. Москва, Рубцовская набережная, д. 4, корп. 3"
-    },
-    branch_3:{
-        geometry:null,
-        iconContent:'',
-        iconCaption:"Филиал №3",
-        preset:"islands#blueCircleDotIconWithCaption",
-        balloonText:"Заведующий филиалом № 3:<br><b>Жидков Сергей Георгиевич</b>",
+        balloonText:"Заведующий филиалом «Фемида»:<br><b>Романенков Андрей Федорович</b>",
         link:"",
         balloonImg:"",
-        comment:"Филиал № 3, г. Москва, ?????"
-    },
-    branch_4:{
-        geometry:[55.742448, 37.546390],
-        iconContent:'',
-        iconCaption:"Филиал №4",
-        preset:"islands#blueCircleDotIconWithCaption",
-        balloonText:"Заведующий филиалом № 4:<br><b>Денисов Сергей Иванович</b>",
-        link:"",
-        balloonImg:"",
-        comment:"Филиал № 4, г. Москва, ул.  Студенческая, 26/3 стр.1"
-    },
-    branch_5:{
-        geometry:[56.185873, 36.977101],
-        iconContent:'',
-        iconCaption:"Филиал №5",
-        preset:"islands#blueCircleDotIconWithCaption",
-        balloonText:"Заведующий филиалом № 5:<br><b>Миронов Анатолий Николаевич</b>",
-        link:"http://mkasol.ru",
-        balloonImg:"",
-        comment:"Филиал № 5, Московская обл., г. Солнечногорск, ул. Советская, д.9, кв.7"
-    },
-    branch_8:{
-        geometry:[55.976398, 37.178700],
-        iconContent:'',
-        iconCaption:"Филиал №8",
-        preset:"islands#blueCircleDotIconWithCaption",
-        balloonText:"Заведующий филиалом № 8<br><b>Ряховский Олег Александрович</b>",
-        link:"",
-        balloonImg:"",
-        comment:"Филиал № 8, гор. Зеленоград, Заводская 16 а, офис 3"
-    },
-    branch_18:{
-        geometry:[55.859802, 37.664860],
-        iconContent:'',
-        iconCaption:"Филиал №18",
-        preset:"islands#blueCircleDotIconWithCaption",
-        balloonText:"Заведующий филиалом № 18:<br><b>Охрименко Дмитрий Александрович</b>",
-        link:"",
-        balloonImg:"",
-        comment:"Филиал № 18, гор. Москва, ул. Радужная, д.11 кв.61"
+        comment:"гор. Москва, ул. Куусинена д.19А каб. 303; Основная связь по телефонам: +79051629660 и +79152962929; эл. почта: andrrom@mail.ru; Понедельник-пятница с 09.00 до 18.00 (без обеда)"
     },
     branch_interlawer:{
         geometry:[55.720672, 37.417643],
@@ -69,15 +19,65 @@ const coordinates = {
         balloonImg:"",
         comment:"Филиал Интерлоер, гор. Москва,  ул. Кутузова, д. 2,/111"
     },
-    branch_femida:{
-        geometry:[55.787973, 37.511266],
+    branch_18:{
+        geometry:[55.859802, 37.664860],
         iconContent:'',
-        iconCaption:"Филиал Фемида",
+        iconCaption:"Филиал №18",
         preset:"islands#blueCircleDotIconWithCaption",
-        balloonText:"Заведующий филиалом «Фемида»:<br><b>Романенков Андрей Федорович</b>",
+        balloonText:"Заведующий филиалом № 18:<br><b>Охрименко Дмитрий Александрович</b>",
         link:"",
         balloonImg:"",
-        comment:"гор. Москва, ул. Куусинена д.19А каб. 303; Основная связь по телефонам: +79051629660 и +79152962929; эл. почта: andrrom@mail.ru; Понедельник-пятница с 09.00 до 18.00 (без обеда)"
+        comment:"Филиал № 18, гор. Москва, ул. Радужная, д.11 кв.61"
+    },
+    branch_8:{
+        geometry:[55.976398, 37.178700],
+        iconContent:'',
+        iconCaption:"Филиал №8",
+        preset:"islands#blueCircleDotIconWithCaption",
+        balloonText:"Заведующий филиалом № 8<br><b>Ряховский Олег Александрович</b>",
+        link:"",
+        balloonImg:"",
+        comment:"Филиал № 8, гор. Зеленоград, Заводская 16 а, офис 3"
+    },
+    branch_5:{
+        geometry:[56.185873, 36.977101],
+        iconContent:'',
+        iconCaption:"Филиал №5",
+        preset:"islands#blueCircleDotIconWithCaption",
+        balloonText:"Заведующий филиалом № 5:<br><b>Миронов Анатолий Николаевич</b>",
+        link:"http://mkasol.ru",
+        balloonImg:"",
+        comment:"Филиал № 5, Московская обл., г. Солнечногорск, ул. Советская, д.9, кв.7"
+    },
+    branch_4:{
+        geometry:[55.742448, 37.546390],
+        iconContent:'',
+        iconCaption:"Филиал №4",
+        preset:"islands#blueCircleDotIconWithCaption",
+        balloonText:"Заведующий филиалом № 4:<br><b>Денисов Сергей Иванович</b>",
+        link:"",
+        balloonImg:"",
+        comment:"Филиал № 4, г. Москва, ул.  Студенческая, 26/3 стр.1"
+    },
+    branch_3:{
+        geometry:null,
+        iconContent:'',
+        iconCaption:"Филиал №3",
+        preset:"islands#blueCircleDotIconWithCaption",
+        balloonText:"Заведующий филиалом № 3:<br><b>Жидков Сергей Георгиевич</b>",
+        link:"",
+        balloonImg:"",
+        comment:"Филиал № 3, г. Москва, ?????"
+    },
+    branch_2:{
+        geometry:[55.775819, 37.698538],
+        iconContent:'',
+        iconCaption:"Филиал №2",
+        preset:"islands#blueCircleDotIconWithCaption",
+        balloonText:"Заведующая филиалом № 2:<br><b>Трегуб Ольга Анатольевна</b>",
+        link:"",
+        balloonImg:"https://avatars.mds.yandex.net/get-altay/859900/2a0000015f57ecdcf9997bcd4ad7e1420b57/XXXL",
+        comment:"Филиал № 2, г. Москва, Рубцовская набережная, д. 4, корп. 3"
     },
     branch_1:{
         geometry:[55.767379, 37.584293],
