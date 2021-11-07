5f3ef4f46ab2ce1eed2f3a5f36b315ddf5e73924
Ax51 <80718359+Ax51@users.noreply.github.com>
2021-11-08 01:12:51 +0300

    Contacts page now ready for mobile devices

--- src/Components/Pages/Contacts-page/Contacts-page.js
+++ src/Components/Pages/Contacts-page/Contacts-page.js
@@ -1,5 +1,5 @@
 // modules
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 // Components
 import MapApi from "../../Map-API/Map-API.js";
@@ -10,16 +10,22 @@ import NavLine from "../../Nav-line/Nav-line.js";
 import './Contacts-page.css';
 
 const ContactsPage = () => {
-    let [ branchCode, setBranchCode ] = useState(8);
+    const [branchCode, setBranchCode] = useState(8);
+    const contactsRef = useRef(null);
 
     let selectedBranch = Db[Object.keys(Db)[branchCode]];
+
+    function chooseBranch(num) {
+        setBranchCode(num);
+        contactsRef.current.scrollIntoView({ block: 'start', behavior: 'smooth' });
+    }
 
     let buttons = Object.keys(Db).map((item, num) => {
         return (
             <button
                 key={Db[item].iconCaption}
                 className={branchCode === num ? "btn btn-dark active" : "btn btn-dark"}
-                onClick={() => setBranchCode(num)}>
+                onClick={() => chooseBranch(num)}>
                 {Db[item].iconCaption || Db[item].iconContent}
             </button>
         )
@@ -28,24 +34,33 @@ const ContactsPage = () => {
         <>
             <div className="contacts-page">
                 <NavLine
-                    pathArray={[{name:"Контакты"}]} />
+                    pathArray={[{ name: "Контакты" }]} />
                 <div className="contacts-page_header">
                     <h1>Реквизиты наших филиалов</h1>
-                    <div className="header-block header-block_dark"/>
+                    <div className="header-block header-block_dark" />
                 </div>
                 <div className="contacts-page_wrapper">
                     <div className='contacts-page_buttons'>
                         {buttons}
                     </div>
                     <div className="contacts-page_contacts">
+                        <div className="contacts-page_contacts_anchor" ref={contactsRef} /> {/* scroll anchor */}
                         <h2>{selectedBranch.iconCaption || selectedBranch.iconContent}</h2>
-                        <div className="header-block header-block_dark" style={{marginBottom:"15px"}}/>
-                        {selectedBranch.comment}
+                        <div className="header-block header-block_dark" />
+                        {/* {selectedBranch.comment} */}
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veritatis placeat repellendus, sequi eum voluptas molestiae magni minima consequuntur voluptates dolores eaque atque labore quisquam vel ea quis iusto in accusamus numquam cupiditate? In excepturi libero sapiente consectetur quia dolore quos, esse vel tempore consequatur officiis laudantium repellendus magnam ratione!
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, placeat dolorum!</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+                            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, aspernatur.</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, vel?</li>
+                        </ul>
                     </div>
                 </div>
             </div>
             <MapApi
-                branchCode={branchCode} />
+                branchCode={branchCode} /> 
         </>
     )
 }
